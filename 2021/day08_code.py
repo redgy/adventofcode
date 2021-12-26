1f9a1c40d74eb46e4d521e3a6f11f374526c87d9
Redgy Canos <reginacanos@gmail.com>
2021-12-26 00:22:15 +0000

    [wip] Add contains and rework class for other numbers

--- 2021/day08_code.py
+++ 2021/day08_code.py
@@ -1,52 +1,67 @@
 INPUT_FILEPATH="day08_input.txt"
 INPUT_FILEPATH="test_input.txt"
-MAP_WIRES= {
+MAP_PATTERNS= {
     1: 2,
     4: 4,
     7: 3,
     8: 7
 }
-class UniqueDigit:
-    # 1, 4, 7, 8
+class DigitalNumber:
     def __init__(self, number, signal_pattern=''):
-        if number == 1:
-            self.num_unique_signal_pattern = MAP_WIRES[1]
-        elif number == 4:
-            self.num_unique_signal_pattern = MAP_WIRES[4]
-        elif number == 7:
-            self.num_unique_signal_pattern = MAP_WIRES[7]
-        elif number == 8:
-            self.num_unique_signal_pattern = MAP_WIRES[8]
-        else:
-            raise ValueError(f'{number} is not a unique digit')
         self.number = number
+        self.length_of_code = len(signal_pattern)
         self.signal_pattern = self.set_signal_pattern(signal_pattern) if signal_pattern else ''
+        print(self)
 
     def __str__(self):
         return f'[{self.number}]: {self.signal_pattern}'
+
+    def __contains__(self, other):
+        """Does number contains pattern from other number"""
+        for pattern_bit in other.signal_pattern:
+            if not pattern_bit in self.signal_pattern:
+                return False
+        return True
 
     def set_signal_pattern(self, signal_pattern):
         """Set signal_pattern for digit, e.g. 1=>ab"""
 
         if len(signal_pattern) != self.num_unique_signal_pattern:
             raise ValueError(f'{signal_pattern} is the incorrect length of {self.num_unique_signal_pattern}')
-        return signal_pattern
+        pattern_list = list(signal_pattern)
+        pattern_list.sort()
+        return pattern_list
 
 
 class DebugDisplay:
     def __init__(self, data):
         self.data = data
+        self.zero = None
         self.one = None
+        self.two = None
+        self.three = None
         self.four = None
+        self.five = None
+        self.six = None
         self.seven = None
         self.eight = None
+        self.nine = None
 
-    def _is_unique_digits_initialized(self):
-        return self.one and self.four and self.seven and self.eight
+    def reset_numbers(self):
+        self.zero = None
+        self.one = None
+        self.two = None
+        self.three = None
+        self.four = None
+        self.five = None
+        self.six = None
+        self.seven = None
+        self.eight = None
+        self.nine = None
 
     def get_unique_digit_occurrences(self):
         count = 0
-        unique_pattern_array = MAP_WIRES.values()
+        unique_pattern_array = MAP_PATTERNS.values()
         for line in self.data:
             digit_array = line[1]
             for digit in digit_array:
@@ -54,24 +69,28 @@ class DebugDisplay:
                     count += 1
         return count
 
-    def get_signal_patterns(self):
+    def get_digital_output(self):
+        digital_outputs = []
         for line in self.data:
-            if self._is_unique_patterns_initialized():
-                break
             signal_patterns = line[0]
             self.set_unique_pattern(signal_patterns)
             print(signal_patterns)
+            break
 
     def set_unique_pattern(self, signal_patterns):
         for signal_pattern in signal_patterns:
-            if len(signal_pattern) == MAP_WIRES[1]:
+            if len(signal_pattern) == MAP_PATTERNS[1]:
                 self.one = UniqueDigit(1, signal_pattern)
-            elif len(signal_pattern) == MAP_WIRES[4]:
+            elif len(signal_pattern) == MAP_PATTERNS[4]:
                 self.four = UniqueDigit(4, signal_pattern)
-            elif len(signal_pattern) == MAP_WIRES[7]:
+            elif len(signal_pattern) == MAP_PATTERNS[7]:
                 self.seven = UniqueDigit(7, signal_pattern)
-            elif len(signal_pattern) == MAP_WIRES[8]:
+            elif len(signal_pattern) == MAP_PATTERNS[8]:
                 self.eight = UniqueDigit(8, signal_pattern)
+            elif len(signal_pattern) == 5:  # 2, 3, 5
+                pass
+            elif len(signal_pattern) == 6:  # 0, 6, 9
+                pass
 
 class InputData:
     def __init__(self):
