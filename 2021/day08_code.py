030b746cb07371b0a257d07348579619751f3186
Redgy Canos <reginacanos@gmail.com>
2021-12-26 02:29:10 +0000

    Rework logic and return sum of all outputs

--- 2021/day08_code.py
+++ 2021/day08_code.py
@@ -17,6 +17,9 @@ class SignalPattern:
     def __str__(self):
         return self.str
 
+    def __eq__(self, other):
+        return self.code == other.code
+
     def contains(self, other):
         for pattern_bit in other.code:
             if not pattern_bit in self.code:
@@ -69,6 +72,13 @@ class DebugDisplay:
                     count += 1
         return count
 
+    def get_sum_of_outputs(self):
+        digital_outputs = self.get_digital_outputs()
+        total = 0
+        for x in digital_outputs:
+            total += x
+        return total
+
     def _print_digits(self):
         print(self.zero)
         print(self.one)
@@ -102,21 +112,20 @@ class DebugDisplay:
             return self.eight.number
         if signal_pattern == self.nine.signal_pattern:
             return self.nine.number
-        raise RuntimeError('Did we finish deduction?')
+        raise RuntimeError(f'Did we finish deduction?')
 
-    def get_digital_output(self):
+    def get_digital_outputs(self):
         digital_outputs = []
         for line in self.data:
             signal_pattern_strings = [SignalPattern(x).str for x in line[0]]
             self.set_pattern(signal_pattern_strings)
-            self._print_digits()
+            # self._print_digits()
             encoded_outputs = line[1]
-            for encoded_output in encoded_outputs:
-                encoded_pattern = SignalPattern(encoded_output)
-                decoded_digit = self._decode_pattern(encoded_pattern)
-                print(decoded_digit)
+            decoded_array = [self._decode_pattern(SignalPattern(x)) for x in encoded_outputs]
+            decoded_digit = int(''.join(str(n) for n in decoded_array))
+            digital_outputs.append(decoded_digit)
             self.reset_numbers()
-            break
+        return digital_outputs
 
     def set_pattern(self, signal_pattern_strings):
         # Get unique patterns first (1, 4, 7, 8)
@@ -128,11 +137,11 @@ class DebugDisplay:
         signal_pattern_strings.remove(self.seven.signal_pattern.str)
         signal_pattern_strings.remove(self.eight.signal_pattern.str)
 
-        # Next numbers that can be deduced are 5, 6
+        # Next numbers that can be deduced are 3, 6
         for pattern_string in signal_pattern_strings:
             signal_pattern = SignalPattern(pattern_string)
-            self._set_pattern_for_five_and_six(signal_pattern)
-        signal_pattern_strings.remove(self.five.signal_pattern.str)
+            self._set_pattern_for_three_and_six(signal_pattern)
+        signal_pattern_strings.remove(self.three.signal_pattern.str)
         signal_pattern_strings.remove(self.six.signal_pattern.str)
 
         # Next numbers that can be deduced are 0, 9
@@ -142,12 +151,12 @@ class DebugDisplay:
         signal_pattern_strings.remove(self.zero.signal_pattern.str)
         signal_pattern_strings.remove(self.nine.signal_pattern.str)
 
-        # Last numbers to be deduced are 2, 3
+        # Last numbers to be deduced are 2, 5
         for pattern_string in signal_pattern_strings:
             signal_pattern = SignalPattern(pattern_string)
-            # Number 3 will be found by checking number 9 contains 3's code
-            if not self.nine.signal_pattern.contains(signal_pattern):
-                self.three = DigitalNumber(3, signal_pattern)
+            # Number 5 will be found by checking number 9 contains 5's code
+            if self.nine.signal_pattern.contains(signal_pattern):
+                self.five = DigitalNumber(5, signal_pattern)
             # Number 2 is the remaining one
             else:
                 self.two = DigitalNumber(2, signal_pattern)
@@ -165,24 +174,24 @@ class DebugDisplay:
         elif length_of_code == MAP_PATTERNS[8]:
             self.eight = DigitalNumber(8, signal_pattern)
 
-    def _set_pattern_for_five_and_six(self, signal_pattern):
+    def _set_pattern_for_three_and_six(self, signal_pattern):
         if signal_pattern.length == 5:
-            # Number 5 is the only code length 5 that contains 7's code
-            if signal_pattern.contains(self.seven.signal_pattern):
-                self.five = DigitalNumber(5, signal_pattern)
+            # Number 3 is the only code length 5 that contains 1's code
+            if signal_pattern.contains(self.one.signal_pattern):
+                self.three = DigitalNumber(3, signal_pattern)
         elif signal_pattern.length == 6:
-            # Number 6 is the only one code length 6 that does not contain 7's code
-            if not signal_pattern.contains(self.seven.signal_pattern):
+            # Number 6 is the only one code length 6 that does not contain 1's code
+            if not signal_pattern.contains(self.one.signal_pattern):
                 self.six = DigitalNumber(6, signal_pattern)
 
     def _set_pattern_for_zero_and_nine(self, signal_pattern):
         if signal_pattern.length == 6:
-            # Number 0 does not contain 5's code
-            if not signal_pattern.contains(self.five.signal_pattern):
-                self.zero = DigitalNumber(0, signal_pattern)
-            # Number 9 contains both 5 and 7's code
-            if signal_pattern.contains(self.five.signal_pattern) and signal_pattern.contains(self.seven.signal_pattern):
+            # Number 9 contains 3's code
+            if signal_pattern.contains(self.three.signal_pattern):
                 self.nine = DigitalNumber(9, signal_pattern)
+            # Number 0 is the other number
+            else:
+                self.zero = DigitalNumber(0, signal_pattern)
 
 
 class InputData:
@@ -209,8 +218,8 @@ def main():
     data = InputData()
     debug = DebugDisplay(data.input_data)
     count = debug.get_unique_digit_occurrences()
-    debug.get_digital_output()
-    print(f'[!!] Unique digit count: {count}')
+    output_sum = debug.get_sum_of_outputs()
+    print(f'[!!] Sum of all digital outputs: {output_sum}')
 
 
 main()
