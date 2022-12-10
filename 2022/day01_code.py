22ef472090a3dab0a3f03e335b6bb2412bf6cc94
Redgy Canos <redgy@users.noreply.github.com>
2022-12-10 17:55:23 +0000

    Add day 1 part 1

---
+++ 2022/day01_code.py
@@ -0,0 +1,97 @@
+from utils import get_input
+from utils import pp
+
+FILENAME = 'day01_input_sample.txt'
+# FILENAME = 'day01_input.txt'
+
+
+class Elf:
+    def __init__(self, number, calorie_list_string):
+        """Constructor
+
+        :int number: Numbered elf from input
+        :list<str> calorie_list_string: List of calories elf is holding
+        """
+
+        self._number = number
+        self._calorie_list = [int(x) for x in calorie_list_string]
+        self._calorie_sum = sum(self._calorie_list)
+
+    def __str__(self):
+        """To string method"""
+
+        return (
+            f"ELF #{self._number}\n"
+            f"   Calorie list: {self._calorie_list}\n"
+            f"   Calorie sum: {self._calorie_sum}"
+        )
+
+    def __repr__(self):
+        return (
+            f"ELF #{self._number}\n"
+            f"   Calorie list: {self._calorie_list}\n"
+            f"   Calorie sum: {self._calorie_sum}"
+        )
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def calorie_list(self):
+        return self._calorie_list
+
+    @property
+    def calorie_sum(self):
+        return self._calorie_sum
+
+
+def get_data():
+    """Get data from input"""
+
+    return get_input(FILENAME, 'basic')
+
+
+def initialize_elves(data):
+    """Initialize the list of elves from input
+
+    :param list data: List of data from input; includes an entry for empty lines
+    :returns: List of Elf objects
+    """
+
+    elves = []
+    calorie_list = []
+    elf_number = 1
+    for entry in data:
+        if entry  == '':
+            elves.append(Elf(elf_number, calorie_list))
+            calorie_list = []
+            elf_number += 1
+            continue
+        calorie_list.append(entry)
+    return elves
+
+
+def part_one(elves):
+    """Part one is finding the elf holding the most calories
+
+    :param list<Elf> elves: List of Elf objects
+    :returns: Elf. The elf holding the most calories
+    """
+
+    max_elf = elves[0]
+    for elf in elves:
+        if elf.calorie_sum > max_elf.calorie_sum:
+            max_elf = elf
+    return max_elf
+
+
+def main():
+    data = get_data()
+    elves = initialize_elves(data)
+    max_elf = part_one(elves)
+    pp.pprint(max_elf)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2022/utils.py
@@ -0,0 +1,19 @@
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
+
+def get_basic_input(filename):
+    """Basic input expected that requires no additional action besides a simple read in"""
+
+    with open(filename, 'r') as f:
+        data = f.readlines()
+    data = [x.strip() for x in data]
+    return data
+
+
+def get_input(filename, input_type='basic'):
+    match input_type:
+        case 'basic':
+            return get_basic_input(filename)
+        case _:
+            raise ValueError(f'{input_type} not supported')
