c68b03090a7fff02108825912f2fe173c2decbdc
rcanos <Redgy.Canos@viasat.com>
2023-12-04 16:32:07 -0800

    Write code to pass tests

--- 2023/days/day01.py
+++ 2023/days/day01.py
@@ -1,5 +1,14 @@
 from utils import get_file_contents
 
 
-def calibrate(line: str):
-    return line
+def calibrate(line: str) -> int:
+    """Take a string return the first and last digit"""
+    integers = [c for c in line if c.isdigit()]
+    num_string = ""
+    if not integers:
+        num_string = 0
+    elif len(integers) == 1:
+        num_string = f'{integers[0]}{integers[0]}'
+    else:
+        num_string = f'{integers[0]}{integers[-1]}'
+    return int(num_string)
