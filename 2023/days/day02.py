a48b2750e0b95618926a0eef9e3dbf2cc398d9d0
rcanos <Redgy.Canos@viasat.com>
2023-12-05 12:18:16 -0800

    Read in data and see how it looks

--- 2023/days/day02.py
+++ 2023/days/day02.py
@@ -1,11 +1,25 @@
 # TITLE: Cube Conundrum
 from utils import get_file_contents, plog
 INPUT_FILE = 'samples/day02.txt'
+RED = 12
+BLUE = 14
+GREEN = 13
+
+
+def strip_list(some_list):
+    return [x.strip() for x in some_list]
 
 
 def get_max_game_data(game_record: str) -> dict:
     """From game record, create max game data"""
-    return {}
+    game_id_string, all_pulls_string = game_record.split(':')
+    game_id = int(game_id_string.split(' ')[1])  # ['Game', 'N']
+    max_data = {'red': 0, 'blue': 0, 'green': 0}
+    pull_list = strip_list(all_pulls_string.split(';'))
+    for cubes_pulled in pull_list:
+        single_cubes = strip_list(cubes_pulled.split(','))
+        print(single_cubes)
+    return {game_id: max_data}
 
 
 def is_game_possible(max_game_data: dict, starting_cubes: dict) -> bool:
@@ -17,9 +31,10 @@ def puzzle_one(raw_data: list) -> int:
     """What is the sum of the IDs of possible games?"""
     game_records = [get_max_game_data(x) for x in raw_data]
     possible_games = []
+    max_data = {'red': RED, 'blue': BLUE, 'green': GREEN}
     for record in game_records:
-        for game_id, max_data in record.items():
-            if is_game_possible(max_data):
+        for game_id, game_data in record.items():
+            if is_game_possible(game_data, max_data):
                 possible_games.append(game_id)
     return sum(possible_games)
 
