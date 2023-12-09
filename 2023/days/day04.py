8b2a74cc7ed146475806c254377f2833b74d71e1
rcanos <redgycanos@gmail.com>
2023-12-09 08:39:00 -0800

    Stub out methods for tests

--- 2023/days/day04.py
+++ 2023/days/day04.py
@@ -4,6 +4,21 @@ import re
 INPUT_FILE = 'input/day03.txt'
 
 
+def get_matching_winning_numbers(your_numbers: list, winning_numbers: list) -> list:
+    """Compare two lists and get winning matches
+
+    :param your_numbers: List of your numbers
+    :param winning_numbers: List of winning numbers
+    :returns: List of matching winning numbers
+    """
+    return []
+
+
+def calculate_points(winning_numbers: list) -> int:
+    """From a list of matching winning numbers, calculate the points"""
+    return -1
+
+
 def puzzle_one(raw_data: list) -> int:
     pass
 
---
+++ 2023/tests/test_04.py
@@ -0,0 +1,12 @@
+from days.day04 import get_matching_winning_numbers, calculate_points
+# from copy import deepcopy
+
+
+class TestMatchingWinningNumbers:
+    def test(self):
+        pass
+
+
+class TestCalculatePoints:
+    def test(self):
+        pass
