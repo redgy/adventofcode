8416f65c9ed4ceab7ea019bc7128e70a03f88c7e
rcanos <redgycanos@gmail.com>
2023-12-09 09:12:18 -0800

    Finish puzzle one

--- 2023/days/day04.py
+++ 2023/days/day04.py
@@ -1,7 +1,7 @@
 # TITLE: Scratchcards
-from utils import get_file_contents, plog
+from utils import get_file_contents, clean_list, convert_str_to_int, plog
 import re
-INPUT_FILE = 'input/day03.txt'
+INPUT_FILE = 'input/day04.txt'
 
 
 def get_matching_winning_numbers(your_numbers: list, winning_numbers: list) -> list:
@@ -22,13 +22,33 @@ def calculate_points(winning_numbers: list) -> int:
     if len(winning_numbers) == 1:
         return 1
     points = 1
-    for num in winning_numbers[1:]:
+    for _ in winning_numbers[1:]:
         points += points
     return points
 
 
+def parse_numbers(line: str) -> (list, list):
+    """Parse line and get your numbers / winning numbers"""
+    raw_numbers = line.split(':')[1]
+    split_numbers = raw_numbers.split('|')
+    raw_your_numbers = split_numbers[0]
+    raw_winning_numbers = split_numbers[1]
+
+    your_numbers = clean_list(raw_your_numbers.split(' '))
+    your_numbers = [convert_str_to_int(x) for x in your_numbers]
+    winning_numbers = clean_list(raw_winning_numbers.split(' '))
+    winning_numbers = [convert_str_to_int(x) for x in winning_numbers]
+    return your_numbers, winning_numbers
+
+
 def puzzle_one(raw_data: list) -> int:
-    pass
+    """How many points are the scratchcards worth in total?"""
+    total = 0
+    for line in raw_data:
+        your_numbers, winning_numbers = parse_numbers(line)
+        matching_points = get_matching_winning_numbers(your_numbers, winning_numbers)
+        total += calculate_points(matching_points)
+    return total
 
 
 def puzzle_two(raw_data: list) -> int:
@@ -38,8 +58,8 @@ def puzzle_two(raw_data: list) -> int:
 if __name__ == "__main__":
     raw_data = get_file_contents(INPUT_FILE)
 
-    # result = puzzle_one(raw_data)
-    # plog(result)
+    result = puzzle_one(raw_data)
+    plog(result)
 
     # result = puzzle_two(raw_data)
     # plog(result)
--- 2023/days/utils.py
+++ 2023/days/utils.py
@@ -4,7 +4,19 @@ from pprint import PrettyPrinter
 def get_file_contents(filename: str, content_type="string") -> list:
     with open(filename, 'r') as f:
         data = f.readlines()
-    return [x.strip() for x in data]
+    return clean_list(data)
+
+
+def clean_list(some_list: list) -> list:
+    return [x.strip() for x in some_list if x]
+
+
+def convert_str_to_int(character: str) -> int:
+    try:
+        number = int(character)
+    except ValueError as e:
+        raise(e)
+    return number
 
 
 def plog(contents: str):
