1c7ba64944dd9eca99575f9a338fd94fcd6ce8aa
rcanos <redgycanos@gmail.com>
2023-12-09 08:32:51 -0800

    Add initial day 4

---
+++ 2023/days/day04.py
@@ -0,0 +1,22 @@
+# TITLE: Scratchcards
+from utils import get_file_contents, plog
+import re
+INPUT_FILE = 'input/day03.txt'
+
+
+def puzzle_one(raw_data: list) -> int:
+    pass
+
+
+def puzzle_two(raw_data: list) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    raw_data = get_file_contents(INPUT_FILE)
+
+    # result = puzzle_one(raw_data)
+    # plog(result)
+
+    # result = puzzle_two(raw_data)
+    # plog(result)
