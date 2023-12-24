ecd663e981525912428ba3178380d6272ee35add
rcanos <redgycanos@gmail.com>
2023-12-24 11:55:35 -0800

    I give up on day 5

--- 2023/days/day05.py
+++ 2023/days/day05.py
@@ -1,11 +1,12 @@
 # TITLE: If You Give A Seed A Fertilizer
 from utils import get_file_contents, clean_list, convert_str_to_int, plog
 import re
-INPUT_FILE = 'samples/day05.txt'
+INPUT_FILE = 'input/day05.txt'
 
 
 def parse_data(raw_data: list) -> dict:
-    seed_list = raw_data[0]
+    first_row = raw_data[0]  # "seeds: N, N, N, N"
+    seed_list = first_row.split(' ')[1:]
     parsed_map = {'seeds': seed_list}
     raw_map_data = clean_list(raw_data[2:])
     raw_map_data.reverse()  # go backwards
@@ -14,7 +15,8 @@ def parse_data(raw_data: list) -> dict:
         is_not_map_title = row.find('map:') == -1
         if is_not_map_title:
             convert_numbers = row.split(' ')
-            numbers_list.extend(convert_numbers)
+            int_numbers = [int(x) for x in convert_numbers]
+            numbers_list.append(int_numbers)
         else:
             map_title = row.split(' ')[0]
             parsed_map[map_title] = numbers_list
@@ -27,9 +29,22 @@ def create_map(destination_start: int, source_start: int, range_length: int) -> 
     return {source_start+x: destination_start+x for x in range(range_length)}
 
 
+def create_almanac(parsed_data: dict):
+    almanac = {k: {} for k in parsed_data.keys()}
+    for key, nested_list in parsed_data.items():
+        if key == 'seeds':
+            continue
+        key_map = {}
+        for triplet in nested_list:
+            new_map = create_map(triplet[0], triplet[1], triplet[2])
+            key_map.update(new_map)
+        almanac[key] = key_map
+    return almanac
+
+
 def get_from_almanac(almanac_key: str, dict_key: int, almanac: dict):
     """Get value from almanac given a key (e.g. seed-to-soil) and dict key (e.g. 12)"""
-    return almanac[almanac_key].get(dict_key)
+    return almanac[almanac_key].get(dict_key, dict_key)
 
 
 def get_soil(starting_seed: int, almanac: dict):
@@ -80,8 +95,15 @@ def get_location(starting_seed: int, almanac: dict):
 
 
 def puzzle_one(raw_data: list) -> int:
-    """TODO"""
-    pass
+    """What is the lowest location number that corresponds to any of the initial seed numbers?"""
+    parsed_data = parse_data(raw_data)
+    seeds = parsed_data['seeds']
+    almanac = create_almanac(parsed_data)
+    locations = []
+    for seed in seeds:
+        location = get_location(seed, almanac)
+        locations.append(int(location))
+    return min(locations)
 
 
 def puzzle_two(raw_data: list) -> int:
