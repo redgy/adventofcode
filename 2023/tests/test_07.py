09ae0c3d2c66ecc35c76a7d7ae7c95382985d582
rcanos <Redgy.Canos@viasat.com>
2023-12-29 15:34:27 -0800

    Lower numbers == weaker hands; change logic in tests

--- 2023/tests/test_07.py
+++ 2023/tests/test_07.py
@@ -5,19 +5,19 @@ class TestHandType:
     def test_five_of_a_kind(self):
         mock_hand = '55555'
         actual = get_hand_type(mock_hand)
-        expected = 1
+        expected = 7
         assert actual == expected
 
     def test_four_of_a_kind(self):
         mock_hand = '4444T'
         actual = get_hand_type(mock_hand)
-        expected = 2
+        expected = 6
         assert actual == expected
 
     def test_full_house(self):
         mock_hand = 'KKKAA'
         actual = get_hand_type(mock_hand)
-        expected = 3
+        expected = 5
         assert actual == expected
 
     def test_three_of_a_kind(self):
@@ -29,19 +29,19 @@ class TestHandType:
     def test_two_pair(self):
         mock_hand = 'QTQT9'
         actual = get_hand_type(mock_hand)
-        expected = 5
+        expected = 3
         assert actual == expected
 
     def test_one_pair(self):
         mock_hand = '88123'
         actual = get_hand_type(mock_hand)
-        expected = 6
+        expected = 2
         assert actual == expected
 
     def test_high_card(self):
         mock_hand = 'AKQJT'
         actual = get_hand_type(mock_hand)
-        expected = 7
+        expected = 1
         assert actual == expected
 
 
@@ -50,33 +50,33 @@ class TestCompareHands:
         mock_one = '53343'
         mock_two = '72228'
         actual = compare_hands(mock_one, mock_two)
-        expected = mock_two
+        expected = mock_one
         assert actual == expected
 
     def test_second_card(self):
         mock_one = 'A9999'
         mock_two = 'AA876'
         actual = compare_hands(mock_one, mock_two)
-        expected = mock_two
+        expected = mock_one
         assert actual == expected
 
     def test_third_card(self):
         mock_one = '435JT'
         mock_two = '43TTJ'
         actual = compare_hands(mock_one, mock_two)
-        expected = mock_two
+        expected = mock_one
         assert actual == expected
 
     def test_fourth_card(self):
         mock_one = '992QK'
         mock_two = '992K7'
         actual = compare_hands(mock_one, mock_two)
-        expected = mock_two
+        expected = mock_one
         assert actual == expected
 
     def test_fifth_card(self):
         mock_one = '34567'
         mock_two = '34568'
         actual = compare_hands(mock_one, mock_two)
-        expected = mock_two
+        expected = mock_one
         assert actual == expected
